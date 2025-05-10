d427c6bfd7520dc28fd6c6ad4f6d363ca00c7a25
Parthiangold <nathfann01@gmail.com>
2025-05-10 12:20:15 +1000

    Working on Booking.java

--- src/main/java/org/example/Booking.java
+++ src/main/java/org/example/Booking.java
@@ -1,4 +1,20 @@
 package org.example;
+import java.util.*;
 
 public class Booking {
+    private int adultAmount;
+    private int childAmount;
+    private double price;
+    private String bookingDate;
+    private String dateTime;
+    private String email;
+    private int bookingNo;
+    private int customerNo;
+    private Flight bookedFlight;
+    private String[] bookedSeats;
+    private boolean inFlightWiFi;
+
+
+    //Empty constructor class
+
 }
