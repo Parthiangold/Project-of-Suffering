2ff5ba75c8d1d814c0495973fb2e390725fe0f4c
Parthiangold <nathfann01@gmail.com>
2025-05-24 16:06:25 +1000

    Added new class to be the main staging class for JFX UI

---
+++ src/main/java/group/Application.java
@@ -0,0 +1,21 @@
+package group;
+
+import java.util.*;
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.util.*;
+import javafx.animation.*;
+
+public class Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
